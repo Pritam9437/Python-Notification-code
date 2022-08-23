a008082baeb7d5ec14da3ed7d1058aad9927756f
Pritam <kabitanjalinayak1981@gmail.com>
2022-08-23 22:33:29 +0530

    First commit

---
+++ notification.py
@@ -0,0 +1,10 @@
+import time
+from plyer import notification
+if __name__ == "__main__":
+
+    notification.notify(
+        title="drink water",
+        message="to avoid disease",
+
+        timeout=20
+    )
